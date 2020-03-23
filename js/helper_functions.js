e4547cf18b2ba45011c428843d325504f8f0fdd1
liteburn <arsik996@gmail.com>
2020-03-23 05:48:58 +0200

    scoreboard added

--- js/helper_functions.js
+++ js/helper_functions.js
@@ -1,23 +1,29 @@
-function checkMove(param1, param2) {
+function checkMove(param1, param2)
+{
     let canBe = Boolean(true);
     renderPlayground();
     object.position[object.pos].forEach(position => (playground[position[0]][position[1]] = undefined));
     object.position[object.pos].forEach(position => (((position[0] + param1 < 0 || (9 < position[1] + param2 || position[1] + param2 < 0)) && (canBe = false))));
-    if (canBe){
+    if (canBe)
+    {
 
         object.position[object.pos].forEach(position => ((playground[position[0] + param1][position[1] + param2] !== undefined) && (canBe = false)));
     }
-    if (!canBe){
+    if (!canBe)
+    {
          renderPlayground();
     }
     return canBe;
 }
 
-function checkAndDelRow() {
-    var delite = [];
-    for (i = 0; i < playground.length; i++){
+function checkAndDelRow()
+{
+    let delite = [];
+    for (i = 0; i < playground.length; i++)
+    {
         del = true;
-        for(k = 0; k < playground[i].length; k++){
+        for(k = 0; k < playground[i].length; k++)
+        {
             if (playground[i][k] === undefined){
                 del = false;
             }
@@ -26,11 +32,21 @@ function checkAndDelRow() {
             delite.push(i);
         }
     }
-    for (i = delite.length - 1; i >= 0; i--){
-        for(k = delite[i]; k < playground.length - 1; k++){
+    for (i = delite.length - 1; i >= 0; i--)
+    {
+        for(k = delite[i]; k < playground.length - 1; k++)
+        {
             playground[k] = playground[k + 1];
         }
         playground[playground.length - 1] = new Array(10).fill();
     }
-
+    let scoreboard = document.getElementById("scoreboard");
+    let scores = Number(scoreboard.innerText);
+    let bonus = 1;
+    if (delite.length === 4)
+    {
+        bonus = 2;
+    }
+    scores += delite.length * 10 * bonus;
+    scoreboard.innerText = scores;
 }
--- js/tetris.js
+++ js/tetris.js
@@ -3,7 +3,8 @@ let object = createObj();
 let object1;
 pause = false;
 
-// will add object positions to the emply playground array
+
+//will add new object to partly filled playground.
 function renderPositions() {
     if (object){
     object.position[object.pos].forEach(([rowIndex, cellIndex]) => {
@@ -12,112 +13,125 @@ function renderPositions() {
     }
 }
 
-function moveDown(obj) {
-  renderPlayground();
-  // 1. get current object - done
 
-  // 2. re-define objects - done
-  if (checkMove( -1, 0)) {
+function moveDown(obj)
+{
+  renderPlayground();
+
+  if (checkMove( -1, 0))
+  {
       object.position.forEach(position => position.forEach(position => (position[0] -= 1)));
   }
-  else{
-    object =  createObj();
-    checkAndDelRow();
-    renderPositions();
+  else
+  {
+      object =  createObj();
+      checkAndDelRow();
+      renderPositions();
 
   }
   renderPlayground();
 }
 
-function moveLeft(obj) {
+
+function moveLeft(obj)
+{
   renderPlayground();
-  if (checkMove(0, 1)) {
+  if (checkMove(0, 1))
+  {
       object.position.forEach(position => position.forEach(position => (position[1] += 1)));
-
-
-      // 4. re-renderPositions
-      // 5. re-renderPlayground
       renderPlayground();
   }
-
-
-
 }
+
 
 function moveRight(obj) {
   renderPlayground();
     if (checkMove(0, -1)){
       object.position.forEach(position => position.forEach(position => (position[1] -= 1)));
-
-
-      // 4. re-renderPositions
-      // 5. re-renderPlayground
       renderPlayground();
   }
 
 }
 
-function changeFigure(){
+
+function changeFigure()
+{
+    let ob;
     let add = true;
-    for (i = 0; i < object.position[object.pos].length; i++){
+    for (i = 0; i < object.position[object.pos].length; i++)
+    {
         ob = object.position[object.pos][i];
         playground[ob[0]][ob[1]] = undefined;
     }
-    for (i = 0; i < object.position[(object.pos + 1) % object.position.length].length; i++) {
-        var ob = object.position[(object.pos + 1) % object.position.length][i];
-        if (playground[ob[0]][ob[1]] !== undefined || ob[0] < 0 || ob[1] < 0 || ob[1] > 9 || ob[0] > 19) {
+    for (i = 0; i < object.position[(object.pos + 1) % object.position.length].length; i++)
+    {
+        ob = object.position[(object.pos + 1) % object.position.length][i];
+        if (playground[ob[0]][ob[1]] !== undefined || ob[0] < 0 || ob[1] < 0 || ob[1] > 9 || ob[0] > 19)
+        {
             add = false;
         }
     }
-
-    if (add){
+    if (add)
+    {
         object.pos = (object.pos + 1)%object.position.length;
     }
     renderPlayground();
 }
 
 
-function pauseGame() {
-  if (pause === false){
+function pauseGame()
+{
+  if (pause === false)
+  {
       object1 = object;
       clearInterval(gameInterval);
       pause = true;
       object = false;
   }
-  else{
+  else
+  {
       pause = false;
       object = object1;
-     gameInterval = setInterval(() => {
-    moveDown();
+      gameInterval = setInterval(() => {
+      moveDown();
 
-}, 500);
+      }     , 500);
   }
-
 }
 
-function createObj() {
-  function getRandomNum(max) {
+
+function createObj()
+{
+  function getRandomNum(max)
+  {
       return Math.floor(Math.random() * Math.floor(max));
-    }
-    figure = FIGURES[getRandomNum(FIGURES.length)];
-    obj = ({type: figure, position: [] , state: 'falling', pos: 0});
-    for (i = 0; i < INITIAL_POSITIONS[figure].length; i++){
-        obj.position.push([]);
-        for(k = 0; k < INITIAL_POSITIONS[figure][i].length; k++){
+  }
+  figure = FIGURES[getRandomNum(FIGURES.length)];
+  obj = ({type: figure, position: [] , state: 'falling', pos: 0});
+  for (i = 0; i < INITIAL_POSITIONS[figure].length; i++)
+  {
+      obj.position.push([]);
+      for(k = 0; k < INITIAL_POSITIONS[figure][i].length; k++)
+      {
 
-            if (playground[INITIAL_POSITIONS[figure][i][k][0]][INITIAL_POSITIONS[figure][i][k][1]] !== undefined){
+          if (playground[INITIAL_POSITIONS[figure][i][k][0]][INITIAL_POSITIONS[figure][i][k][1]] !== undefined)
+          {
                 gameOver();
-            };
-            obj.position[i].push([...INITIAL_POSITIONS[figure][i][k]]);
-        }
-    }
-    num = getRandomNum(7);
-    obj.position.forEach(position => position.forEach(position => position[1] += num));
-    return obj;
+          }
+          obj.position[i].push([...INITIAL_POSITIONS[figure][i][k]]);
+      }
+  }
+  num = getRandomNum(7);
+  obj.position.forEach(position => position.forEach(position => position[1] += num));
+  return obj;
 }
 
-function gameOver() {
-   window.location.href = "game_over.html";
+
+function gameOver()
+{
+    obj = false;
+    setTimeout(function() { window.location.href = "game_over.html";}, 3000);
+
 }
 
 // Events
